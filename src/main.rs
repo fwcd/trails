5fff3fb8e687d1c237a601bfe17d50a9d5d523ce
fwcd <fwcdmail@gmail.com>
2022-07-05 22:49:52 +0200

    Use a bigger window

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@ use druid::{AppLauncher, WindowDesc, Widget, widget::Label};
 
 fn main() {
     let window = WindowDesc::new(build_ui)
-        .title("Trails");
-    let data = ();
+        .title("Trails")
+        .window_size((800.0, 600.0));
+
+    let initial_state = ();
+
     AppLauncher::with_window(window)
-        .launch(data)
+        .launch(initial_state)
         .expect("Failed to launch app");
 }
 
