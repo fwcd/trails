f85e6e94e3b83e5ff5dcd5c00eb4fe82fd4f1bdb
fwcd <fwcdmail@gmail.com>
2022-07-05 22:46:32 +0200

    Set up a minimal GUI app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use druid::{AppLauncher, WindowDesc, Widget, widget::Label};
+
 fn main() {
-    println!("Hello, world!");
+    let window = WindowDesc::new(build_ui)
+        .title("Trails");
+    let data = ();
+    AppLauncher::with_window(window)
+        .launch(data)
+        .expect("Failed to launch app");
+}
+
+fn build_ui() -> impl Widget<()> {
+    Label::new("Hello world")
 }
