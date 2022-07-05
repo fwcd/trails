2ea6a66895e32dfb9c577f9b6ec917244c580d13
fwcd <fwcdmail@gmail.com>
2022-07-05 23:03:36 +0200

    Add an address bar

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,34 @@
-use druid::{AppLauncher, WindowDesc, Widget, widget::Label};
+use std::sync::Arc;
+
+use druid::{AppLauncher, WindowDesc, Widget, widget::{Label, Flex, TextBox}, WidgetExt, Data, Lens};
+
+#[derive(Clone, Data, Lens)]
+struct AppState {
+    url: Arc<String>,
+}
 
 fn main() {
     let window = WindowDesc::new(build_ui)
         .title("Trails")
         .window_size((800.0, 600.0));
 
-    let initial_state = ();
+    let initial_state = AppState {
+        url: Arc::new("".to_owned()),
+    };
 
     AppLauncher::with_window(window)
         .launch(initial_state)
         .expect("Failed to launch app");
 }
 
-fn build_ui() -> impl Widget<()> {
-    Label::new("Hello world")
+fn build_ui() -> impl Widget<AppState> {
+    Flex::column()
+        // Address bar
+        .with_child(
+            TextBox::new()
+                .with_placeholder("Enter URL...")
+                .lens(AppState::url)
+                .expand_width()
+                .padding(10.0)
+        )
 }
